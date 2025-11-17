16d27d7fe84998d6d71d577a11d598cc27092391
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-17 12:43:02 +0500

    updated

---
+++ goai/src/assets/Images/Image.jsx
@@ -0,0 +1,23 @@
+
+const Images = {
+  logo: new URL("./goai-logo.png", import.meta.url).href,
+  heroimage: new URL("./hero-image.webp", import.meta.url).href,
+  orbit1: new URL("./orbit1.png", import.meta.url).href,
+  orbit2: new URL("./orbit2.png", import.meta.url).href,
+  orbit3: new URL("./orbit3.png", import.meta.url).href,
+  orbit4: new URL("./orbit4.png", import.meta.url).href,
+  ranks: new URL("./rank-image.webp", import.meta.url).href,
+  smallicon1: new URL("./smallicon1.png", import.meta.url).href,
+  smallicon2: new URL("./smallicon2.png", import.meta.url).href,
+  smallicon3: new URL("./smallicon3.png", import.meta.url).href,
+  smallicon4: new URL("./smallicon4.png", import.meta.url).href,
+  smallicon5: new URL("./smallicon5.png", import.meta.url).href,
+  smallicon6: new URL("./smallicon6.png", import.meta.url).href,
+  smallicon7: new URL("./smallicon7.png", import.meta.url).href,
+  smallicon8: new URL("./smallicon8.png", import.meta.url).href,
+  smallicon9: new URL("./smallicon9.png", import.meta.url).href,
+  smallicon10: new URL("./smallicon10.webp", import.meta.url).href,
+  smallicon11: new URL("./smallicon11.webp", import.meta.url).href,
+  smallicon12: new URL("./smallicon12.webp", import.meta.url).href,
+};
+export default Images;
