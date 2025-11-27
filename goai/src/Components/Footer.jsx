b8eb7930b4ca3c7d87ae1b6f351b6da9b62701ad
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-27 12:57:41 +0500

    update

--- goai/src/Components/Footer.jsx
+++ goai/src/Components/Footer.jsx
@@ -14,17 +14,14 @@ export default function Footer() {
               className="text-center text-lg-start text-white"
               style={{ backgroundColor: "#1c2331" }}
             >
-  
               <section className="d-flex justify-content-between "></section>
-         
+
               <section className="">
                 <div className="container text-center text-md-start mt-5">
-                 
                   <div className="row mt-3">
-                    
                     <div className="col-md-3 col-lg-6 col-xl-6 mx-auto mb-4">
                       {/* Content */}
-                      
+
                       <img
                         src={Images.logo}
                         alt="logo"
@@ -33,32 +30,60 @@ export default function Footer() {
                       />
 
                       <p className="mt-3">
-                        GO Ai exists to merge financial education, trading
+                        GO AI exists to merge financial education, trading
                         technology, and human potential into a single ecosystem
                         of empowerment...
                       </p>
                       <div>
-                        <Link to="https://www.facebook.com/profile.php?id=61583860222236" className="text-white me-4" target="_blank">
+                        <Link
+                          to="https://www.facebook.com/profile.php?id=61583860222236"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-facebook-f" />
                         </Link>
-                         <Link to="https://www.instagram.com/goai.global/" className="text-white me-4"target="_blank">
+                        <Link
+                          to="https://www.instagram.com/goai.global/"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-instagram" />
                         </Link>
-                        <Link to="https://x.com/GOAI_GLOBAL" className="text-white me-4"target="_blank">
+                        <Link
+                          to="https://x.com/GOAI_GLOBAL"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-twitter" />
                         </Link>
-                         <Link to="https://www.linkedin.com/in/go-ai-global-258758398/" className="text-white me-4"target="_blank">
+                        <Link
+                          to="https://www.linkedin.com/in/go-ai-global-258758398/"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-linkedin" />
                         </Link>
-                        <Link to="http://www.youtube.com/@GOAIGLOBAL" className="text-white me-4"target="_blank">
+                        <Link
+                          to="http://www.youtube.com/@GOAIGLOBAL"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-youtube" />
                         </Link>
-                       
-                          <Link to="https://www.tiktok.com/@goai.global" className="text-white me-4"target="_blank">
+
+                        <Link
+                          to="https://www.tiktok.com/@goai.global"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-tiktok" />
                         </Link>
-                       
-                        <Link to="https://t.me/GOAIGLOBAL" className="text-white me-4"target="_blank">
+
+                        <Link
+                          to="https://t.me/GOAIGLOBAL"
+                          className="text-white me-4"
+                          target="_blank"
+                        >
                           <i className="fab fa-telegram" />
                         </Link>
                       </div>
@@ -97,8 +122,7 @@ export default function Footer() {
                         </Link>
                       </p>
                     </div>
-                    {/* Grid column */}
-                    {/* Grid column */}
+
                     <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                       {/* Links */}
                       <h6 className="text-uppercase fw-bold">Contact</h6>
@@ -111,8 +135,8 @@ export default function Footer() {
                         }}
                       />
                       <p>
-                        <i className="fas fa-home mr-3" /> GO AI, LLC • Wyoming, USA
-                        
+                        <i className="fas fa-home mr-3" /> GO AI, LLC 30 N Gould
+                        St. #57397 Sheridan, WY, 82801
                       </p>
                       <p>
                         <i className="fas fa-envelope mr-3" /> info@goai.com
@@ -134,14 +158,13 @@ export default function Footer() {
               >
                 © 2025&nbsp;
                 <a className="text-white" to="https://mdbootstrap.com/">
-                  GOAI
+                  GO AI, LLC
                 </a>
               </div>
               {/* Copyright */}
             </footer>
             {/* Footer */}
           </div>
-          {/* End of .container */}
         </>
       </>
     </>
