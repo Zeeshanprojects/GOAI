6d08321662c3edbc071ccd93ea3a0385b41954d9
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-28 22:26:59 +0500

    update

--- goai/src/Pages/RefundPolicy.jsx
+++ goai/src/Pages/RefundPolicy.jsx
@@ -25,7 +25,7 @@ export default function RefundPolicy() {
 
           {/* MAIN CONTENT */}
           <div className="terms-content text-white">
-            <h3 className="terms-heading mt-5">Summary</h3>
+            <h3 className="terms-heading mt-5">1.&nbsp;Summary</h3>
             <ul>
               <li>
                 <p>
@@ -46,7 +46,7 @@ export default function RefundPolicy() {
                 </p>
               </li>
             </ul>
-            <h3 className="terms-heading mt-5">What’s Eligible</h3>
+            <h3 className="terms-heading mt-5">2.&nbsp;What’s Eligible</h3>
             <p>We may issue a refund within 7 days of purchase for:</p>
             <ul>
               <li>
@@ -65,7 +65,7 @@ export default function RefundPolicy() {
               <strong>Note:</strong> “7 days” means 7 consecutive calendar days
               starting on the day of purchase.
             </p>
-            <h3 className="terms-heading mt-5">Not Eligible</h3>
+            <h3 className="terms-heading mt-5">3.&nbsp;Not Eligible</h3>
             <p>We do not provide refunds for:</p>
             <ul>
               <li>
@@ -96,7 +96,7 @@ export default function RefundPolicy() {
                 <p>Requests submitted after the 7-day window</p>
               </li>
             </ul>
-            <h3 className="terms-heading mt-5">How to Request a Refund</h3>
+            <h3 className="terms-heading mt-5">4.&nbsp;How to Request a Refund</h3>
             <ul>
               <li>
                 Email<strong>support@goai.global</strong> from the account used
@@ -122,7 +122,7 @@ export default function RefundPolicy() {
             
               <li>Requests submitted after the 7-day window.</li>
             </ul>
-            <h3 className="terms-heading mt-5">Cancellations</h3>
+            <h3 className="terms-heading mt-5">5.&nbsp;Cancellations</h3>
             <p>
               You can cancel subscriptions at any time to prevent future
               charges. Cancellation takes effect at the end of the current
@@ -130,7 +130,7 @@ export default function RefundPolicy() {
               provided (except where required by law).
             </p>
 
-            <h3 className="terms-heading mt-5">Chargebacks & Disputes</h3>
+            <h3 className="terms-heading mt-5">6.&nbsp;Chargebacks & Disputes</h3>
             <p>
               Please contact us first to resolve any issues. Filing a chargeback
               without first working with us may result in immediate account
@@ -139,12 +139,12 @@ export default function RefundPolicy() {
               financial institutions as “friendly fraud” and may result in a
               strike on your credit report. Please adhere to the refund process.{" "}
             </p>
-            <h3 className="terms-heading mt-5">Fraud & Abuse</h3>
+            <h3 className="terms-heading mt-5">7.&nbsp;Fraud & Abuse</h3>
             <p>
               We reserve the right to refuse a refund if we suspect fraud,
               refund abuse, or violations of our Terms of Use.
             </p>
-            <h3>Legal</h3>
+               <h3 className="terms-heading mt-5">8.&nbsp;Legal</h3>
             <p>
               Nothing in this policy limits any non-waivable consumer rights
               available to you under applicable law. GO AI may update this
