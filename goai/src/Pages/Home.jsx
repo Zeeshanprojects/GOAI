fdc899b087b2468e2a53bb3b68ea7c43f4d59974
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-28 22:40:41 +0500

    update

--- goai/src/Pages/Home.jsx
+++ goai/src/Pages/Home.jsx
@@ -289,7 +289,7 @@ export default function Home() {
           <h1 className="title">Mission</h1>
 
           <p>
-           <p> To reinvent wealth through intelligence</p>
+            To reinvent wealth through intelligence<br/>
             GO AI exists to merge financial education, trading technology, and
             human potential into a single ecosystem of empowerment. We engineer
             prosperity through community, automation, and data-driven precision.
